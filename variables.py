f129edb3620c90c16aade47543b1506700991a21
saumyajaiswal2805 <78247155+saumyajaiswal2805@users.noreply.github.com>
2021-02-14 17:19:53 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,3 +2,10 @@ print("I am SAUMYA JAISWAL,")
 print("and i am 23 years old,")
 print("currently a student of JNU,")
 print("stream DATA SCIENCE.")
+C:\Users\user\PycharmProjects\pythonProject\venv\Scripts\python.exe C:/Users/user/PycharmProjects/pythonProject/variables.py
+I am SAUMYA JAISWAL,
+and i am 23 years old,
+currently a student of JNU,
+stream DATA SCIENCE.
+
+Process finished with exit code 0
